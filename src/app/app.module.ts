35fb54b780b73d8562c2136626db29ca9d39055e
Marce <jmarce_83@hotmail.com>
2020-11-01 02:08:26 -0300

    first draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CvComponent } from './cv/cv.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CvComponent
   ],
   imports: [
     BrowserModule,
