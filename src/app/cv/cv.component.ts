e019d11c72db026b2f3a5db2d5671077fbc86aad
Marcelo <marcelo.martin@softvision.com>
2020-11-02 16:51:37 -0300

    layout ok

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cv.component.css']
 })
 export class CvComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
